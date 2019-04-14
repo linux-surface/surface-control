84605716b0499c821854aee8d969654844cbe918
qzed <qzed@users.noreply.github.com>
2019-04-14 18:15:12 +0200

    Improve UX
    Provide better feed-back to users. This can be disabled (e.g. for
    scripts) via the `--quiet` flag.

--- src/main.rs
+++ src/main.rs
@@ -31,10 +31,9 @@ fn cmd_status(_: &clap::ArgMatches) -> Result<()> {
     let perf_mode  = sys::perf::Device::open()?.get_mode()?;
     let dgpu_power = sys::dgpu::Device::open()?.get_power()?;
 
-    println!("System Status:");
-    println!("  Device-Mode:      {}", opmode);
-    println!("  Performance-Mode: {}", perf_mode);
-    println!("  dGPU-Power:       {}", dgpu_power);
+    println!("       Device-Mode: {}", opmode);
+    println!("  Performance-Mode: {} ({})", perf_mode, perf_mode.short_str());
+    println!("        dGPU-Power: {}", dgpu_power);
 
     Ok(())
 }
@@ -52,11 +51,24 @@ fn cmd_dgpu_set(m: &clap::ArgMatches) -> Result<()> {
     use clap::value_t_or_exit;
     let state = value_t_or_exit!(m, "state", sys::dgpu::PowerState);
 
-    sys::dgpu::Device::open()?.set_power(state)
+    sys::dgpu::Device::open()?.set_power(state)?;
+
+    if !m.is_present("quiet") {
+        println!("dGPU power set to '{}'", state);
+    }
+
+    Ok(())
 }
 
-fn cmd_dgpu_get(_: &clap::ArgMatches) -> Result<()> {
-    println!("{}", sys::dgpu::Device::open()?.get_power()?);
+fn cmd_dgpu_get(m: &clap::ArgMatches) -> Result<()> {
+    let state = sys::dgpu::Device::open()?.get_power()?;
+
+    if !m.is_present("quiet") {
+        println!("dGPU power is '{}'", state);
+    } else {
+        println!("{}", state);
+    }
+
     Ok(())
 }
 
@@ -71,13 +83,26 @@ fn cmd_perf(m: &clap::ArgMatches) -> Result<()> {
 
 fn cmd_perf_set(m: &clap::ArgMatches) -> Result<()> {
     use clap::value_t_or_exit;
-    let state = value_t_or_exit!(m, "mode", sys::perf::Mode);
+    let mode = value_t_or_exit!(m, "mode", sys::perf::Mode);
 
-    sys::perf::Device::open()?.set_mode(state)
+    sys::perf::Device::open()?.set_mode(mode)?;
+
+    if !m.is_present("quiet") {
+        println!("Performance-mode set to '{}'", mode);
+    }
+
+    Ok(())
 }
 
-fn cmd_perf_get(_: &clap::ArgMatches) -> Result<()> {
-    println!("{}", sys::perf::Device::open()?.get_mode()?);
+fn cmd_perf_get(m: &clap::ArgMatches) -> Result<()> {
+    let mode = sys::perf::Device::open()?.get_mode()?;
+
+    if !m.is_present("quiet") {
+        println!("Performance-mode is '{}' ({})", mode, mode.short_str());
+    } else {
+        println!("{}", mode.short_str());
+    }
+
     Ok(())
 }
 
@@ -92,19 +117,44 @@ fn cmd_latch(m: &clap::ArgMatches) -> Result<()> {
     }
 }
 
-fn cmd_latch_lock(_: &clap::ArgMatches) -> Result<()> {
-    sys::latch::Device::open()?.latch_lock()
+fn cmd_latch_lock(m: &clap::ArgMatches) -> Result<()> {
+    sys::latch::Device::open()?.latch_lock()?;
+
+    if !m.is_present("quiet") {
+        println!("Clipboard latch locked");
+    }
+
+    Ok(())
 }
 
-fn cmd_latch_unlock(_: &clap::ArgMatches) -> Result<()> {
-    sys::latch::Device::open()?.latch_unlock()
+fn cmd_latch_unlock(m: &clap::ArgMatches) -> Result<()> {
+    sys::latch::Device::open()?.latch_unlock()?;
+
+    if !m.is_present("quiet") {
+        println!("Clipboard latch unlocked");
+    }
+
+    Ok(())
 }
 
-fn cmd_latch_request(_: &clap::ArgMatches) -> Result<()> {
-    sys::latch::Device::open()?.latch_request()
+fn cmd_latch_request(m: &clap::ArgMatches) -> Result<()> {
+    sys::latch::Device::open()?.latch_request()?;
+
+    if !m.is_present("quiet") {
+        println!("Clipboard latch request executed");
+    }
+
+    Ok(())
 }
 
-fn cmd_latch_get_opmode(_: &clap::ArgMatches) -> Result<()> {
-    println!("{}", sys::latch::Device::open()?.get_opmode()?);
+fn cmd_latch_get_opmode(m: &clap::ArgMatches) -> Result<()> {
+    let opmode = sys::latch::Device::open()?.get_opmode()?;
+
+    if !m.is_present("quiet") {
+        println!("Device is in '{}' mode", opmode);
+    } else {
+        println!("{}", opmode);
+    }
+
     Ok(())
 }
